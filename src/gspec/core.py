9764599229963c3ab2a24d453fbe50180fbaf69b
Chris Walther Andersen <christian.walther.andersen@rsyd.dk>
2021-10-21 14:32:08 +0200

    Implementing spectrum class

--- src/gspec/core.py
+++ src/gspec/core.py
@@ -1,3 +1,34 @@
+from typing import DefaultDict
+from collections import defaultdict
+from typing import List
+
+
 class spectrum:
 
-	energy_units = 'keV'
+	def __init__(self):
+		self.energy_units = 'keV'
+		self.spectrum_data: DefaultDict[int, float] = defaultdict(float)
+
+		self.count_time = 0.0
+		self.count_time_units = 's'
+
+	# Get the total number of counts in a specified window of the spectrum
+	def get_counts(self, energies: List[int]):
+		sum = 0.0
+		for e in energies:
+			sum += self.spectrum_data[e]
+
+		return sum
+
+	# Set the number of counts in a specified bin of the spectrum
+	def set_counts(self, energy: int, counts: float):
+		self.spectrum_data[energy] = counts
+
+	def subtract_spectrum(self, spec='spectrum'):
+		if self.count_time != spec.count_time:
+			msg = "Different count_time values (" + str(self.count_time)
+			msg += " vs " + str(spec.count_time) + "). "
+			msg += "Set force=True to enforce subtraction."
+			raise ValueError(msg)
+		for e, c in spec.spectrum_data.items():
+			self.spectrum_data[e] -= c
--- test/test_core.py
+++ test/test_core.py
@@ -7,3 +7,60 @@ class TestSpectrum(unittest.TestCase):
 	def test_default_energy_units(self):
 		s = gspec.spectrum()
 		self.assertEqual(s.energy_units, 'keV')
+
+	def test_default_count_time(self):
+		s = gspec.spectrum()
+		self.assertEqual(s.count_time, 0.0)
+		self.assertEqual(s.count_time_units, 's')
+
+	def test_default_data(self):
+		s = gspec.spectrum()
+		self.assertEqual(len(s.spectrum_data), 0)
+
+	def test_get_counts_no_data(self):
+		s = gspec.spectrum()
+		self.assertEqual(s.get_counts([510, 511, 512]), 0)
+
+	def test_set_counts(self):
+		s = gspec.spectrum()
+		s.set_counts(509, 32.1)
+		s.set_counts(510, 10.1)
+		s.set_counts(511, 11.1)
+		self.assertEqual(s.get_counts([510, 511, 512]), 21.2)
+
+	def test_subtraction(self):
+		s1 = gspec.spectrum()
+		s1.set_counts(509, 32.1)
+		s1.set_counts(510, 10.1)
+		s1.set_counts(511, 11.1)
+
+		s2 = gspec.spectrum()
+		s2.set_counts(508, 1.0)
+		s2.set_counts(509, 1.1)
+		s2.set_counts(510, 1.2)
+
+		s1.subtract_spectrum(s2)
+		self.assertEqual(s1.get_counts([508]), -1.0)
+		self.assertEqual(s1.get_counts([509]), 31.0)
+		self.assertEqual(s1.get_counts([510]), 8.9)
+		self.assertEqual(s1.get_counts([511]), 11.1)
+		self.assertEqual(s1.get_counts([512]), 0.0)
+
+		self.assertEqual(s2.get_counts([508]), 1.0)
+		self.assertEqual(s2.get_counts([509]), 1.1)
+		self.assertEqual(s2.get_counts([510]), 1.2)
+		self.assertEqual(s2.get_counts([511]), 0.0)
+
+	def test_subtraction_error_wrong_count_time(self):
+		s1 = gspec.spectrum()
+		s1.set_counts(509, 32.1)
+		s1.set_counts(510, 10.1)
+		s1.set_counts(511, 11.1)
+		s1.count_time = 1.0
+
+		s2 = gspec.spectrum()
+		s2.set_counts(508, 1.0)
+		s2.set_counts(509, 1.1)
+		s2.set_counts(510, 1.2)
+
+		self.assertRaises(ValueError, s1.subtract_spectrum, s2)
