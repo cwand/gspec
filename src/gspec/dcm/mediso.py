ef958dc8ccad1a387671011d6fd523e2d91aa365
Christian Walther Andersen <christian.walther.andersen@rsyd.dk>
2021-12-22 10:44:37 +0100

    Testing mediso-dicom functionality

--- src/gspec/dcm/mediso.py
+++ src/gspec/dcm/mediso.py
@@ -1,66 +1,75 @@
+# type: ignore
+
 import gspec
 import pydicom
 import tempfile
-import numpy as np
+
 
 def read_mediso(fp: str) -> gspec.gspectrum:
 
+	spectrum = gspec.gspectrum()  # This will contain our resulting spectrum
+
 	ds = pydicom.dcmread(fp)
+
+	spectrum.count_time = ds[0x0018, 0x1242].value  # Acquisition time in ms
+	spectrum.count_time_units = 'ms'
+
+	spectrum.energy_units = 'keV'
+
 	file = tempfile.SpooledTemporaryFile()
-	file.write(ds[0x0009,0x10e6].value) # This is where Mediso-private tags are
+	file.write(ds[0x0009, 0x10e6].value)  # This is where Mediso-private tags are
 
 	# Read data from virtual DICOM file
-	med_ds = pydicom.dcmread(file,force=True) # type: ignore
+	med_ds = pydicom.dcmread(file, force=True)
 	# med_ds now contains all Mediso-private tags
 	file.close()
 
 	# Tag (0040,a730) contains a sequence of two items:
 	# One item containing info about the detectors
 	# One item containing info about recorded spectrum
-	x = med_ds[0x0040,0xa730]
+	x = med_ds[0x0040, 0xa730]
 
 	# The contained information is given for each item in the sequence in the tag
 	# (0040,a043)>[Sequence item 0]>(0008,0100)
 	# We find the index of the item containing spectrum information
 	spectrum_index = -1
-	for idx,sq_item in enumerate(x):
-		idx_info = sq_item[0x0040,0xa043]
+	for idx, sq_item in enumerate(x):
+		idx_info = sq_item[0x0040, 0xa043]
 		idx_info = idx_info[0]
-		if idx_info[0x0008,0x0100].value == "Spectrums":
+		if idx_info[0x0008, 0x0100].value == "Spectrums":
 			spectrum_index = idx
 
-	x = x[spectrum_index] # x is now the item in the sequence containing spectrums
+	x = x[spectrum_index]  # x is now the item in the sequence containing spectrums
 
 	# The recorded spectrum is saved in a single item sequence under the tag
 	# (0040,a730)
-	x = x[0x0040,0xa730] # type: ignore
+	x = x[0x0040, 0xa730]
 	x = x[0]
 
 	# Under the tag (0040,a730) is a sequence containing three things:
 	# The spectrum (Spectrum cps by keV) <- What we want
 	# Information about the head
 	# Information about the frameID
-	x = x[0x0040,0xa730] # type: ignore
+	x = x[0x0040, 0xa730]
 	# The contained information is given for each item in the sequence in the tag
 	# (0040,a043)>[Sequence item 0]>(0008,0100)
 	# We find the index of the item containing spectrum information
 	spectrum_index = -1
-	for idx,sq_item in enumerate(x):
-		idx_info = sq_item[0x0040,0xa043]
+	for idx, sq_item in enumerate(x):
+		idx_info = sq_item[0x0040, 0xa043]
 		idx_info = idx_info[0]
-		if idx_info[0x0008,0x0100].value == "Spectrum cps by keV":
+		if idx_info[0x0008, 0x0100].value == "Spectrum cps by keV":
 			spectrum_index = idx
 
-	print(spectrum_index)
-	x = x[spectrum_index] # x is now the item containing the spectrum
+	x = x[spectrum_index]  # x is now the item containing the spectrum
 
 	# The spectrum is saved in the tag (0009,10ea) in utf-8 encoded byte format
-	x = x[0x0009,0x10ea].value # type: ignore
-	s = x.decode('utf-8') # type: ignore
+	x = x[0x0009, 0x10ea].value
+	s = x.decode('utf-8')
 	# s now contains the spectrum as a string
+	# with columns [Energy(keV)  Countrate(cps)]
 
-	
+	# Read the spectrum string line by line
+	bins = s.split('\n')
 
-
-
-	return gspec.gspectrum()
+	return spectrum
--- test/test_mediso.py
+++ test/test_mediso.py
@@ -1,13 +1,19 @@
 import unittest
 import gspec
 from gspec import dcm
-from pathlib import Path
-import numpy as np
 
 
 class TestMediso(unittest.TestCase):
 
 	def test_meas_time(self):
 		s = gspec.dcm.read_mediso('test/00000001.dcm')
-		self.assertEqual(s.count_time, 300)
-		self.assertEqual(s.count_time_units, 's')
+		self.assertEqual(s.count_time, 300000)
+		self.assertEqual(s.count_time_units, 'ms')
+
+	def test_energy_units(self):
+		s = gspec.dcm.read_mediso('test/00000001.dcm')
+		self.assertEqual(s.energy_units, 'keV')
+
+	def test_date(self):
+		s = gspec.dcm.read_mediso('test/00000001.dcm')
+		self.assertEqual(1+1,0)
